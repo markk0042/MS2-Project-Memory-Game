958379e0090f3693d2e5b32a5fc47ba44d9f53ab
Mark Kelly <markk0042@gmail.com>
2020-05-30 12:48:16 +0000

    started JS added functionality to the click to start heading that appears before playing

---
+++ script.js
@@ -0,0 +1,16 @@
+if(document.readState === 'loading'){
+document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
+
+function ready(){
+let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+let cards = Array.from(document.getElementsByClassName('card'));
+
+overlays.forEach(overlay => {
+    overlay.addEventListener('click', () => {
+    overlay.classList.remove('visible');
+});
+});
+};
