18f6499f32ec00b49d08bbebd2b425a9925f86b1
Mark Kelly <markk0042@gmail.com>
2020-06-03 20:00:17 +0000

    updated syntax in JS that casued error in image loading a instead of s in createCard(s.pop()

--- script.js
+++ script.js
@@ -1,55 +1,47 @@
 var gamediv = document.getElementById("Matchinggame");
 
-var cardsize = 100;//created these two for the margin around the image tiles.
+var cardsize = 100;
 var cardspacing = 10;
 var gameWidth = 4;
 var gameHeight = 4;
+var firstcard = null;
+var secondcard = null;
 
-//below I changed x and y value in the loop to h and v for horizontal and vertical,
-//then i placed them into the loop.
-createGrid(gameWidth,gameHeight);
+createGrid(gameWidth, gameHeight);
 
-function createGrid(h,v) {
-    //var a = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];//created array for the cards, but this is not needed now because we created a cleaner code for it below.
-    var a = [];
-    for(var i=0; i<gameWidth*gameHeight/2; i++) {
-        a.push(i);
-        a.push(i);
+function createGrid(h, v) {
+  var a = [];
+  for (var i = 0; i < (gameWidth * gameHeight) / 2; i++) {
+    a.push(i);
+    a.push(i);
+  }
+
+  var s = [];
+  while (a.length > 0) {
+    var r = Math.floor(Math.random() * a.length);
+    s.push(a[r]);
+    a.splice(r, 1);
+  }
+
+  for (var x = 0; x < h; x++) {
+    for (var y = 0; y < v; y++) {
+      createCard(s.pop(), x, y);
     }
-
-    var s = [];
-    while(a.length > 0) {
-        var r = Math.floor(Math.random()*a.length);
-        s.push(a[r]);
-        a.splice(r,1);
-    }
-
-for(var x=0;x<h;x++){
-    for(var y=0;y<v;y++) {
-        createCard(a.pop(),x,y);
-        }
-    }
+  }
 }
 
- 
-//to get the image below to appear multiple times without having to keep writing out code and, 
-//have the chances of a mistake/ error ill use a function of createCard below to create more easier.//
-function createCard(cardNum, posX, posY){
-    var card = document.createElement("img");
-    card.num = cardNum;
-    card.src = "assets/img/cardback.png"; //I concatenated the cardnum so we casnn added different images easier//
-    card.style.position = "absolute";
-    card.style.left = (posX*(cardsize+cardspacing)+cardspacing)+"px";//the equation here will take the 100px wide image and add a 10px margin on the right and then the extra +10px on the left// 
-    card.style.top = (posY*(cardsize+cardspacing)+cardspacing)+"px";
-    card.onclick = clickCard;
-    gamediv.appendChild(card);
+function createCard(cardNum, posX, posY) {
+  var card = document.createElement("img");
+  card.num = cardNum;
+  card.src = "assets/img/cardback.png";
+  card.style.position = "absolute";
+  card.style.left = posX * (cardsize + cardspacing) + cardspacing + "px";
+  card.style.top = posY * (cardsize + cardspacing) + cardspacing + "px";
+  card.onclick = clickCard;
+  gamediv.appendChild(card);
 }
-//posX + posY will be used above in the create card element above the function,
-//this will be called and the position values will be given so we can place the cards,
-//where we want to easier.//
+
 function clickCard(e) {
-	var card = e.target;
-	card.src = "matchcards/card"+card.num+".png"; //adding card.num because card.nums value created is in the create card function and the  
-    //value of create card is a.pop() resulting in the array being pullled.
-    
+  var card = e.target;
+  card.src = "assets/img/card"+ card.num + ".png";
 }
