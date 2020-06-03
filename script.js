344d68a9812b7bd9e9ef8e3a2f677cf9fc3ccfa2
Mark Kelly <markk0042@gmail.com>
2020-06-03 14:10:54 +0000

    added the a.pop() function to the new array in the var we  created and then mentioned the the create grid value which was cardNum which the name of the this was card.num ehich we mentioned in the target for the onclick function and we mentioned the src for the images with this card.num added

--- script.js
+++ script.js
@@ -10,10 +10,12 @@ var gameHeight = 4;
 createGrid(gameWidth,gameHeight);
 
 function createGrid(h,v) {
+    var a = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];//created array for the cards.
+
 for(var x=0; x<h; x++){
     for(var y=0; y<v; y++) {
-        createCard(0, x, y);
-     }
+        createCard(a.pop(),x, y);//pop function will pop off last value in the loop.
+        }
     }
 }
 
@@ -22,12 +24,20 @@ for(var x=0; x<h; x++){
 //have the chances of a mistake/ error ill use a function of createCard below to create more easier.//
 function createCard(cardNum, posX, posY){
     var card = document.createElement("img");
+    card.num = cardNum;
     card.src = "assets/img/cardback.png"; //I concatenated the cardnum so we casnn added different images easier//
     card.style.position = "absolute";
     card.style.left = (posX*(cardsize+cardspacing)+cardspacing)+"px";//the equation here will take the 100px wide image and add a 10px margin on the right and then the extra +10px on the left// 
     card.style.top = (posY*(cardsize+cardspacing)+cardspacing)+"px";
+    card.onclick = clickCard;
     gamediv.appendChild(card);
 }
 //posX + posY will be used above in the create card element above the function,
 //this will be called and the position values will be given so we can place the cards,
 //where we want to easier.//
+function clickCard(e){//used e for event.
+    var card = e.target; 
+    card.src = "assets/img/card"+card.num+".png"; //adding card.num because card.nums value created is in the create card function and the  
+    //value of create card is a.pop() resulting in the array being pullled.
+    
+}
