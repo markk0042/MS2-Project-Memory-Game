d1e84e79eb3cfd5cd1997270152e305b00fe4a0a
Mark Kelly <markk0042@gmail.com>
2020-06-03 13:08:37 +0000

    updatedposition values with X + Y positioning and also concatenated images in the image directory src href in the js file in the function

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 var gamediv = document.getElementById("Matchinggame");
 
-var card = document.createElement("img");
-card.src = "assets/img/card1.png";
-card.style.position = "absolute";
-card.style.left = "10px";
-card.style.top = "10px";
-gamediv.appendChild(card);
+
+createCard(0,0);
+createCard(120,200);
+
+
+
+//to get the image below to appear multiple times without having to keep writing out code and, 
+//have the chances of a mistake/ error ill use a function of createCard below to create more easier.//
+function createCard(cardNum, posX, posY){
+    var card = document.createElement("img");
+    card.src = "assets/img/card"+cardNum+".png"; //I concatenated the cardnum so we casnn added different images easier//
+    card.style.position = "absolute";
+    card.style.left = posX+"px";
+    card.style.top = posY+"px";
+    gamediv.appendChild(card);
+}
+//posX + posY will be used above in the create card element above the function,
+//this will be called and the position values will be given so we can place the cards,
+//where we want to easier.//
